4137bfe09fb69ae43c05dfe8b1e8d4cbae56f145
nikkamille <k.pagibigan@gmail.com>
2020-06-25 16:11:50 -0600

    add more requirements in environment.rb

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,13 @@
-require "anime_list/version"
+require 'anime_list/version'
+require 'pry'
+require 'uri'
+require 'net/http'
+require 'openssl'
+# require "httparty"
+
+require_relative './ApiManager'
+require_relative './anime'
+require_relative './cli'
 
 module AnimeList
   class Error < StandardError; end
