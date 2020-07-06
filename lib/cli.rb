2f185d5d55830ab596c1091007e53d8954ca5233
nikkamille <k.pagibigan@gmail.com>
2020-07-05 23:59:44 -0600

    creates method inside the CLI class

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,10 +3,22 @@ class AnimeList::CLI
   def start
     greeting
     menu
-    get_anime_info
+    get_anime_list
   end
 
+  def greeting
+    puts "\n\nKon'nichiwa! n_n\n\n"
+    sleep(0.5)
+    puts "Welcome to my Anime List!\n\n"
+  end
 
+  def menu
+    puts "in menu"
+  end
+
+  def get_anime_list
+    AnimeList::APIManager.get_anime
+  end
 
 
 end
